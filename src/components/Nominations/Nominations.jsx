703b2ad8d946430c22949e0381243ea12438962a
Ryan Kim <hanjun1@ualberta.ca>
2021-05-05 13:36:32 -0600

    first commit

---
+++ src/components/Nominations/Nominations.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nominations.css";
+
+function Nominations(props) {
+  return (
+    <div className="Nominations">
+      <h1>Nominations</h1>
+    </div>
+  );
+}
+
+export default Nominations;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SearchBar.css";
+
+function SearchBar(props) {
+  return (
+    <div className="SearchBar">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default SearchBar;
